d5a691ac33d19bac41df314065b128e0e618e652
ChaseHarding <chaseharding965@gmail.com>
2024-02-07 19:28:11 -0800

    create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -1,26 +1,28 @@
-const React = require('react')
-const Default = require('./layouts/default')
-const breads = require('../models/bread')
+const React = require("react");
+const Default = require("./layouts/default");
+const breads = require("../models/bread");
 
-function Index ({breads}) {
-    return (
-      <Default>
-        <h2>Index Page</h2>
-        {/* <p>I have {breads[0].name} bread!</p> */}
-        <ul>
-            {
-                breads.map((bread, index) => {
-                    return (
-                    <li key={index}>
-                        <a href={`/breads/${index}`}>
-                            {bread.name}
-                        </a>
-                    </li>)
-                })
-            }
-        </ul>
-      </Default>
-    )
+function Index({ breads }) {
+  return (
+    <Default>
+      <h2>Index Page</h2>
+      {/* <p>I have {breads[0].name} bread!</p> */}
+      <ul>
+        {breads.map((bread, index) => {
+          return (
+            <li key={index}>
+              <a href={`/breads/${index}`}>{bread.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="newButton">
+        <a href="/breads/new">
+          <button>Add a new bread</button>
+        </a>
+      </div>
+    </Default>
+  );
 }
 
-module.exports = Index
+module.exports = Index;
